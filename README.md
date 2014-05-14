dc7af69c312be6c6529506718ea6a35d16fb04a9
K. Godbod <gkounkou@gmail.com>
2014-05-15 00:49:56 +0200

    Plan initial

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 watcher
 =======
 
-The watcher project monitor the system for updates and fills a report file
+The watcher project monitor the system for updates and files a report file
---
+++ main.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+#----------------------------
+# file : main.sh
+#
+# TODO : ... Claudie
+#----------------------------
+
+#-----------------------------------
+# FONCTIONS
+#-----------------------------------
+# lancement tous les jours a 23H
+
+function lancementTache {
+   echo "lancement de la tache"
+   # TODO : ... Claudie
+}
+
+# recuperation de la version du logiciel
+#############################
+# version.txt
+#############################
+
+function recuperationVersion {
+   echo "recuperation de la version"
+   # TODO : ... godbod
+}
+
+# recuperation des connecteurs
+#############################
+# aspen.env
+#############################
+# - processus
+# - port(s)
+# - ip server
+# - etat
+#----------------------------
+
+function recuperationConnecteur {
+   echo "recuperation du connecteur"
+   # TODO : ... godbod
+}
+
+# ecriture du rapport
+#----------------------------
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@
+#
+# VERSION    : xxx
+# CONNECTEUR : xxx
+# ETAT       : xxx
+# IP         : xxx.xxx.xxx.xxx
+# PORT       : xxx
+#
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@
+#----------------------------
+
+function ecritureRapport {
+   echo "ecriture du rapport"
+}
+
+#-----------------------------------
+
+
+#-----------------------------------
+# deroulement programme
+# TODO : ... Claudie
+#-----------------------------------
+
