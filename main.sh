fab8bda4bf948d0f46a082923d7bf41790743014
K. Godbod <gkounkou@gmail.com>
2014-05-16 01:09:40 +0200

    Implementation du createur de rapport

--- main.sh
+++ main.sh
@@ -6,12 +6,19 @@
 # TODO : ... Claudie
 #----------------------------
 
+# variables communes
+VERSION=""
+CONNECTEUR=""
+ETAT=""
+IP=""
+PORT=""
+
 #-----------------------------------
 # FONCTIONS
 #-----------------------------------
 # lancement tous les jours a 23H
 
-function lancementTache {
+lancementTache() {
    echo "lancement de la tache"
    # TODO : ... Claudie
 }
@@ -21,7 +28,7 @@ function lancementTache {
 # version.txt
 #############################
 
-function recuperationVersion {
+recuperationVersion() {
    echo "recuperation de la version"
    # TODO : ... godbod
 }
@@ -36,7 +43,7 @@ function recuperationVersion {
 # - etat
 #----------------------------
 
-function recuperationConnecteur {
+recuperationConnecteur() {
    echo "recuperation du connecteur"
    # TODO : ... godbod
 }
@@ -54,8 +61,34 @@ function recuperationConnecteur {
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@
 #----------------------------
 
-function ecritureRapport {
-   echo "ecriture du rapport"
+ecritureRapport() {
+   echo "ecriture du rapport : debut"
+
+   # Tests Unitaires
+   #~~~~~~~~~~~~~~~~~~~~~~~
+   VERSION="0.0"
+   CONNECTEUR="Laboratoire"
+   ETAT="on"
+   IP="10.10.43.42"
+   PORT="65020"
+   #~~~~~~~~~~~~~~~~~~~~~~~
+   
+   # Si on a des informations on peut afficher
+   if [ VERSION ] || [ CONNECTOR ] || [ ETAT ] || [ IP ] || [ PORT ]
+   then
+      echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@\n"       >> report.txt
+      echo `date +%d/%m/%Y%k:%M`  "\n"           >> report.txt
+      printf "%-12s" "VERSION"    ":$VERSION"    >> report.txt; echo "" >> report.txt
+      printf "%-12s" "CONNECTEUR" ":$CONNECTEUR" >> report.txt; echo "" >> report.txt
+      printf "%-12s" "ETAT"       ":$ETAT"       >> report.txt; echo "" >> report.txt
+      printf "%-12s" "IP"         ":$IP"         >> report.txt; echo "" >> report.txt
+      printf "%-12s" "PORT"       ":$PORT"       >> report.txt; echo "" >> report.txt
+      echo "\n@@@@@@@@@@@@@@@@@@@@@@@@@@@\n"     >> report.txt
+   else
+      echo "Error writing the report"
+      exit 1 
+   fi
+   echo "ecriture du rapport : fin "
 }
 
 #-----------------------------------
@@ -66,3 +99,4 @@ function ecritureRapport {
 # TODO : ... Claudie
 #-----------------------------------
 
+ecritureRapport
