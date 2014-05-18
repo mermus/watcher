32f981440f45b2c26ff82604c8536328e9cebdc8
mermus <claudie.tchinianga@gmail.com>
2014-05-18 19:27:37 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -10,6 +10,7 @@ source ecritureRapport.sh
 source recuperationVersion.sh
 source recuperationConnecteur.sh
 source recuperationModules.sh
+#source recuperationType.sh
 
 # Tests Unitaires
 #~~~~~~~~~~~~~~~~~~~~~~~
@@ -44,11 +45,28 @@ lancementTache() {
 
 #-----------------------------------
 # deroulement programme
+# 1. Se placer sous le compte aXigate 
+#       En executant la commande : "su - login/mdp"
+# 2. Recuperation des elements de monitoring :
+#     - Nom du site "Paris"
+#     - Fonction du serveur "Prod"
+#     - Type du serveur "Linux" : uname
+#     - @IP du serveur "10.10.43.42"
+#     - la Version installer "0.0"
+# 3. Recuperation des modules :
+#     - Nom du module
+#     - @IP sur lequel il est rattache
+#     - SID
+#     - Son Port
+# 4. Recuperation des connecteurs :
+#     - Nom des connecteurs
+#     - Descriptifs et fonctions
 # TODO : ... Claudie
 #-----------------------------------
 
 recuperationVersion $REPERTOIRE_VERSION
-recuperationConnecteur $REPERTOIRE_CONNECTEUR
+recuperationConnecteur $REPERTOIRE_CONNECTEUR 
 recuperationModules $FICHIER_MODULE  
+#recuperationType $TYPE_SERVEUR
 
-ecritureRapport $SITE $NOM $REPERTOIRE_VERSION $TYPE $REPERTOIRE_CONNECTEUR $ETAT $MODULE $IP $SID $PORT
+ecritureRapport $SITE $NOM $REPERTOIRE_VERSION $TYPE $REPERTOIRE_CONNECTEUR $ETAT $MODULE $IP $SID $PORT #$TYPE_SERVEUR
