f52e6f4249b34099b6d334af0b71134d7295b1ff
mermus <claudie.tchinianga@gmail.com>
2014-05-21 10:24:37 +0200

    Update recuperationConnecteur.sh

--- recuperationConnecteur.sh
+++ recuperationConnecteur.sh
@@ -13,8 +13,13 @@ recuperationConnecteur() {
    echo "recuperation du connecteur : debut"
 
    # recuperation du fichier contenant les connecteurs
-   FICHIER=$1
-   echo $FICHIER
+   # REPERTOIRE_CONNECTEUR=$1
+   
+   # cd $PLTDIR/scripts/ : permet de ce placer au niveau du repertoir ou ce trouve le fichier contenant les connecteurs
+   # more                : permet d'afficher à l'écran les informations contenue dans le fichier executable processList
+   # REPERTOIRE_CONNECTEUR=`more $PLTDIR/scripts/processList`
+   FICHIER=$1     # A supprimer pour le test
+   echo $FICHIER  # idem
 
    echo "recuperation du connecteur : fin"
 }
