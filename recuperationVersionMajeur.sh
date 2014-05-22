4817c818f343ee00b4363527cc5e601e3bb14e48
mermus <claudie.tchinianga@gmail.com>
2014-05-22 17:03:28 +0200

    Update recuperationVersionMajeur.sh

--- recuperationVersionMajeur.sh
+++ recuperationVersionMajeur.sh
@@ -16,7 +16,7 @@ recuperationVersionMajeur() {
 
    # recuperation du fichier contenant la version
    VERSION_MAJ=$1
-   VERSION_MAJ=`ls -r /axihome/local/axigate/axilink/templates_fr/versions/versionaxigate/ | cut -f1,2 -d"." | head -1`
+   VERSION_MAJ=`ls -r $PLTDIR/axilink/templates_fr/versions/versionaxigate/ | cut -f1,2 -d"." | head -1`
 
    echo "recuperation de la version majeur : fin"
 }
