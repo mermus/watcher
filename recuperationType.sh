edc06b19425e4ad2bd0d42ba90e2a9ad8a1f46b0
mermus <claudie.tchinianga@gmail.com>
2014-05-23 11:46:02 +0200

    Update recuperationType.sh

--- recuperationType.sh
+++ recuperationType.sh
@@ -8,17 +8,17 @@
 
 # recuperation du Type du serveur
 #############################
-# 
+#
 #############################
 
 recuperationType() {
    echo "recuperation du Type du serveur : debut"
 
    # recuperation du fichier contenant la version
-   $TYPE_SERVEUR=$1
+   TYPE=$1
 
-   # uname : on affiche le Type du serveur 
-   $TYPE_SERVEUR=`uname`
-   
+   # uname : on affiche le Type du serveur
+   TYPE=`uname`
+
    echo "recuperation du Type du serveur : fin"
 }
