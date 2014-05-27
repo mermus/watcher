4b97b868f10ab8173c0600334737f18f7a1065aa
mermus <claudie.tchinianga@gmail.com>
2014-05-28 00:11:03 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,71 @@
 #!/bin/bash
 
 #----------------------------
+# file : ecritureRapport.sh
+#
+# TODO : ... Claudie
+#----------------------------
+
+# ecriture du rapport
+#----------------------------
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@
+#
+# SITE       : xxx
+# NOM        : xxx
+# VERSION    : xxx
+# TYPE       : xxx
+# CONNECTEUR : xxx
+# ETAT       : xxx
+# MODULE     : xxx
+# IP_SERVEUR         : xxx.xxx.xxx.xxx
+# SID        : xxx
+# PORT       : xxx
+#
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@
+#----------------------------
+
+ecritureRapport() {
+   echo "ecriture du rapport : debut"
+
+   # recuperation des parametres
+   SITE=$1
+   NOM=$2
+   VERSION=$3
+   TYPE=$4
+   CONNECTEUR=$5
+   VERSION_MAJ=$6
+   IP_SERVEUR=$7
+   ETAT=$8
+   NOM_SERVEUR=$9
+
+   # Si on a des informations on peut afficher
+   if [ VERSION ] || [ CONNECTOR ] || [ ETAT ] || [ IP_SERVEUR ] || [ SITE ] || [ NOM ] || [ TYPE ] || [ NOM_SERVEUR ] || [ VERSION_MAJ ]
+   then
+      echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@"         >> report.txt
+      echo ""                                    >> report.txt
+      echo `date +%d/%m/%Y%t%k:%M`               >> report.txt
+      printf "%-15s" "SITE"        ":$SITE"       >> report.txt; echo "" >> report.txt
+      printf "%-15s" "NOM"         ":$NOM"        >> report.txt; echo "" >> report.txt
+      printf "%-15s" "VERSION"     ":$VERSION"    >> report.txt; echo "" >> report.txt
+      printf "%-15s" "TYPE"        ":$TYPE"       >> report.txt; echo "" >> report.txt
+      printf "%-15s" "CONNECTEUR"  ":$CONNECTEUR" >> report.txt; echo "" >> report.txt
+      printf "%-15s" "VERSION_MAJ" ":$VERSION_MAJ" >> report.txt; echo "" >> report.txt
+      printf "%-15s" "IP_SERVEUR"  ":$IP"          >> report.txt; echo "" >> report.txt
+      printf "%-15s" "ETAT"        ":$ETAT"       >> report.txt; echo "" >> report.txt
+      printf "%-15s" "NOM_SERVEUR" ":$NOM_SERVEUR" >> report.txt; echo "" >> report.txt
+      echo ""                                    >> report.txt
+      echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@"         >> report.txt
+   else
+      echo "Error writing the report"
+      exit 1
+   fi
+   echo "ecriture du rapport : fin "
+}
+mermus@mermus-K43SJ:~/Téléchargements/git_depot/watcher-master$ 
+mermus@mermus-K43SJ:~/Téléchargements/git_depot/watcher-master$ more main.sh 
+#!/bin/bash
+
+#----------------------------
 # file : main.sh
 #
 # TODO : ... Claudie
@@ -14,29 +79,19 @@ source recuperationType.sh
 source recuperationNom.sh
 source recuperationSite.sh
 source recuperationVersionMajeur.sh
+#source recuperationProcess.sh
 
 # Tests Unitaires
 #~~~~~~~~~~~~~~~~~~~~~~~
 SITE="site"
 NOM="Prod"
-VERSION="0.0"
-# TYPE
-#      0 Linux
-#      1 AIX
-TYPE="0"
+VERSION="version"
+TYPE="TYPE"
 CONNECTEUR="Laboratoire"
-ETAT="on"
-MODULE="Vitale"
-IP="10.10.43.42"
-SID="Gambetta"
-PORT="65020"
-
-REPERTOIRE_VERSION="version"
-REPERTOIRE_CONNECTEUR="processlist"
-FICHIER_MODULE="aspen.env"
-TYPE_SERVEUR="type"
-#NOM_SERVEUR="mon_serveur"
 VERSION_MAJ="version_maj"
+IP="10.10.43.42"
+ETAT="ON_or_OF"
+#REPERTOIRE_CONNECTEUR="processlist"
 
 #~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -74,12 +129,13 @@ lancementTache() {
 # TODO : ... Claudie
 #-----------------------------------
 
-recuperationVersion $REPERTOIRE_VERSION
-recuperationConnecteur $REPERTOIRE_CONNECTEUR
-recuperationModules $FICHIER_MODULE
-recuperationType $TYPE_SERVEUR
-#recuperationNom $NOM_SERVEUR
+recuperationVersion $VERSION
+recuperationType $TYPE
+recuperationConnecteur $CONNECTEUR
 recuperationVersionMajeur $VERSION_MAJ
-#recuperationSite $SITE
+recuperationModules $FICHIER_MODULE
+recuperationNom $NOM
+recuperationSite $SITE
+#recuperationProcess $BACKUP_PROCESS
 
-ecritureRapport $SITE $NOM $REPERTOIRE_VERSION $TYPE_SERVEUR $REPERTOIRE_CONNECTEUR $ETAT $MODULE $IP $SID $PORT $VERSION_MAJ #$NOM_SERVEUR
+ecritureRapport $SITE $NOM $VERSION $TYPE $CONNECTEUR $VERSION_MAJ $IP $ETAT #$MODULE
