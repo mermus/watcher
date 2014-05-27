be7d649040d2f0d78532ae0f773197a08b9e6752
mermus <claudie.tchinianga@gmail.com>
2014-05-28 00:17:17 +0200

    Update recuperationSite.sh

--- recuperationSite.sh
+++ recuperationSite.sh
@@ -17,7 +17,8 @@ recuperationSite() {
    # Permet de savoir si on n est sur un site de TEST / PROD / FORM
    SITE=$1
 
-   SITE=`echo $prompt | cut -f4 -d" "`
+   # SITE=`echo $prompt | cut -f4 -d" "`
+   SITE=`uname -n | cut -f2 -d"-"`
 
    echo "recuperation de la fonction du site : fin"
 }
